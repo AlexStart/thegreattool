0c2a760f38bfdb63a74e48552e43e0d616aa363a
Aliaksei Zhytnik <azhyt@sam-solutions.net>
2016-11-17 13:23:42 +0300

    add basic fields to ProjectMetadata & add for testing ProjectMetadataHelper

--- project/cloud-initializr-adapter/src/main/java/com/sam/jcc/cloud/project/ProjectMetadata.java
+++ project/cloud-initializr-adapter/src/main/java/com/sam/jcc/cloud/project/ProjectMetadata.java
@@ -1,14 +1,49 @@
 /**
- * 
+ *
  */
 package com.sam.jcc.cloud.project;
 
 import com.sam.jcc.cloud.i.project.IProjectMetadata;
+import com.sam.jcc.cloud.i.project.IStatusable;
+import com.sam.jcc.cloud.i.project.Status;
+import lombok.Data;
+
+import java.util.List;
+
+import static com.google.common.base.MoreObjects.toStringHelper;
 
 /**
  * @author Alec Kotovich
- *
  */
-public class ProjectMetadata implements IProjectMetadata {
+@Data
+public class ProjectMetadata implements IProjectMetadata, IStatusable {
 
+    private String bootVersion;
+
+    private String projectName;
+    private String projectType;
+    private String javaVersion;
+    private Boolean webAppPackaging;
+
+    private String groupId;
+    private String artifactId;
+    private String version;
+    private String description;
+
+    private String basePackage;
+
+    private List<String> dependencies;
+
+    private byte[] projectSources;
+
+    private Status status;
+
+    @Override
+    public String toString() {
+        return toStringHelper(getClass())
+                .add("project", projectName)
+                .add("artifactId", artifactId)
+                .add("groupId", groupId)
+                .toString();
+    }
 }
