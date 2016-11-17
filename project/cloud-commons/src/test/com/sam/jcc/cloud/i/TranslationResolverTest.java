e151909204ef5bcac8d5f340886feb7c214affce
Aliaksei Zhytnik <azhyt@sam-solutions.net>
2016-11-17 14:41:30 +0300

    add translation resolver tests

---
+++ project/cloud-commons/src/test/com/sam/jcc/cloud/i/TranslationResolverTest.java
@@ -0,0 +1,61 @@
+package com.sam.jcc.cloud.i;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.AbstractMap.SimpleEntry;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+/**
+ * @author Alexey Zhytnik
+ * @since 17.11.2016
+ */
+public class TranslationResolverTest {
+
+    TranslationResolver resolver = new TranslationResolver();
+
+    @Before
+    public void setUp() {
+        resolver.setUpByResource("translations-test.yml");
+    }
+
+    @Test
+    public void containsProviderNames() {
+        final Map<String, String> names = resolver.getNames(Provider.class);
+
+        assertThat(names).containsOnly(
+                entry("ru", "rus_name"),
+                entry("en", "eng_name")
+        );
+    }
+
+    @Test
+    public void containsProviderDescriptions() {
+        final Map<String, String> descriptions = resolver.getDescriptions(Provider.class);
+
+        assertThat(descriptions).containsOnly(
+                entry("ru", "rus_desc"),
+                entry("en", "eng_desc")
+        );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void failsOnUnknownType() {
+        final TranslationResolver resolver = new TranslationResolver();
+        resolver.setUpByResource("translations-wrong.yml");
+    }
+
+    Entry<String, String> entry(String lang, String value) {
+        return new SimpleEntry<>(lang, value);
+    }
+
+    static abstract class Provider extends AbstractProvider {
+        public Provider(List list) {
+            super(list);
+        }
+    }
+}
