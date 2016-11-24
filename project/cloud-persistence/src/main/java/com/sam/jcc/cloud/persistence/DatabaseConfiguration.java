7e537a6ab13173b63d6187657fd6c1917bd8251d
Aliaksei Zhytnik <azhyt@sam-solutions.net>
2016-11-24 13:30:24 +0300

    add DataSource profiles

--- project/cloud-persistence/src/main/java/com/sam/jcc/cloud/persistence/DatabaseConfiguration.java
+++ project/cloud-persistence/src/main/java/com/sam/jcc/cloud/persistence/DatabaseConfiguration.java
@@ -1,10 +1,7 @@
 package com.sam.jcc.cloud.persistence;
 
 import org.hibernate.jpa.HibernatePersistenceProvider;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -31,7 +28,17 @@ public class DatabaseConfiguration {
     Environment env;
 
     @Bean
-    public DataSource dataSource() {
+    @Profile("!prod")
+    public DataSource inMemoryDataSource() {
+        final DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+        ds.setDriverClassName("org.h2.Driver");
+        return ds;
+    }
+
+    @Bean
+    @Profile("prod")
+    public DataSource prodDataSource() {
         final DriverManagerDataSource ds = new DriverManagerDataSource();
 
         ds.setUrl(env.getRequiredProperty("db.hibernate.url"));
@@ -42,17 +49,17 @@ public class DatabaseConfiguration {
     }
 
     @Bean
-    public JpaTransactionManager transactionManager() {
+    public JpaTransactionManager transactionManager(DataSource dataSource) {
         final JpaTransactionManager txManager = new JpaTransactionManager();
-        txManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        txManager.setEntityManagerFactory(entityManagerFactory(dataSource).getObject());
         return txManager;
     }
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
         final LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
 
-        factory.setDataSource(dataSource());
+        factory.setDataSource(dataSource);
         factory.setPersistenceProviderClass(HibernatePersistenceProvider.class);
         factory.setPackagesToScan("com.sam.jcc.cloud.persistence");
         factory.setJpaProperties(getHibernateProperties());
