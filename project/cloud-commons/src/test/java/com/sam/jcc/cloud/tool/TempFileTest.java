8eb5f444354f93126ef106feff648dafd1c6efb0
Aliaksei Zhytnik <azhyt@sam-solutions.net>
2016-12-01 11:23:07 +0300

    add TempFile tests

---
+++ project/cloud-commons/src/test/java/com/sam/jcc/cloud/tool/TempFileTest.java
@@ -0,0 +1,40 @@
+package com.sam.jcc.cloud.tool;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+/**
+ * @author Alexey Zhytnik
+ * @since 29.11.2016
+ */
+public class TempFileTest {
+
+    @Rule
+    public TemporaryFolder temp = new TemporaryFolder();
+
+    @Test
+    public void removesFileAutomatically() throws IOException {
+        final File file = temp.newFile();
+
+        try (TempFile tmp = new TempFile(file)) {
+            assertThat(tmp.exists()).isTrue();
+        }
+        assertThat(file.exists()).isFalse();
+    }
+
+    @Test
+    public void removesFolderAutomatically() throws IOException {
+        final File file = temp.newFolder();
+
+        try (TempFile tmp = new TempFile(file)) {
+            assertThat(tmp.exists()).isTrue();
+        }
+        assertThat(file.exists()).isFalse();
+    }
+}
