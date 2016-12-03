d470f793c61ee21ae51ea048795f64610a6a8643
Alexey Zhytnik <alexeyzhytnik@gmail.com>
2016-12-03 13:29:30 +0300

    work with zip by zip4j

--- project/cloud-commons/src/main/java/com/sam/jcc/cloud/tool/ZipArchiveManager.java
+++ project/cloud-commons/src/main/java/com/sam/jcc/cloud/tool/ZipArchiveManager.java
@@ -1,20 +1,21 @@
 package com.sam.jcc.cloud.tool;
 
+import net.lingala.zip4j.exception.ZipException;
+import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.util.Zip4jConstants;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.springframework.stereotype.Component;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Enumeration;
+import java.security.SecureRandom;
+import java.util.Random;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
-import java.util.zip.ZipOutputStream;
 
-import static java.util.Objects.isNull;
-import static org.apache.commons.io.IOUtils.copy;
+import static org.apache.commons.io.IOUtils.toByteArray;
 
 /**
  * @author Alexey Zhytnik
@@ -22,6 +23,9 @@ import static org.apache.commons.io.IOUtils.copy;
  */
 @Component
 public class ZipArchiveManager {
+
+    private Random random = new SecureRandom();
+    private FileManager files = new FileManager();
 
     public boolean isZip(File file) {
         return file.getName().endsWith(".zip");
@@ -45,88 +49,35 @@ public class ZipArchiveManager {
     }
 
     public void unzip(File archive, File dest) {
-        try (ZipFile zip = new ZipFile(archive)) {
-            final Enumeration<? extends ZipEntry> entries = zip.entries();
-
-            while (entries.hasMoreElements()) {
-                final ZipEntry e = entries.nextElement();
-
-                if (e.isDirectory()) {
-                    createDirByEntry(dest, e);
-                } else {
-                    createFileByEntry(dest, zip, e);
-                }
-            }
-        } catch (IOException e) {
+        try {
+            new net.lingala.zip4j.core.ZipFile(archive).extractAll(dest.getAbsolutePath());
+        } catch (ZipException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private void createDirByEntry(File dest, ZipEntry entry) throws IOException {
-        final Path path = getPath(entry, dest);
-        Files.createDirectories(path);
-    }
-
-    private void createFileByEntry(File dest, ZipFile zip, ZipEntry entry) throws IOException {
-        try (final InputStream in = new BufferedInputStream(zip.getInputStream(entry))) {
-            final Path path = getPath(entry, dest);
-            Files.createFile(path);
-            try (OutputStream out = new FileOutputStream(path.toFile())) {
-                copy(in, out);
-            }
-        }
-    }
-
-    private Path getPath(ZipEntry entry, File dir) {
-        final String name = entry.getName();
-        final Path path = dir.toPath().resolve(name);
-        final File parent = path.toFile().getParentFile();
-
-        if (!parent.exists()) {
-            try {
-                Files.createDirectories(parent.toPath());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return path;
     }
 
     public byte[] zip(File dir) {
-        final ByteArrayOutputStream buffer = new ByteArrayOutputStream((int) dir.length());
-
-        try (ZipOutputStream zip = new ZipOutputStream(buffer)) {
-            addDir(zip, dir, dir);
-            zip.finish();
-            return buffer.toByteArray();
-        } catch (Exception e) {
+        try {
+            try (TempFile temp = createTempZip()) {
+                new net.lingala.zip4j.core.ZipFile(temp).addFolder(dir, getZipParameters());
+                return toByteArray(temp.toURI());
+            }
+        } catch (ZipException | IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private void addDir(ZipOutputStream zip, File root, File current) throws IOException {
-        File[] files = current.listFiles();
-        if (isNull(files) || files.length == 0) return;
-
-        for (File file : files) {
-            if (file.isDirectory()) {
-                addDir(zip, root, file);
-                continue;
-            }
-            createZipEntry(zip, root, file);
-        }
+    private TempFile createTempZip() {
+        final String name = Long.toString(Math.abs(random.nextLong()));
+        final TempFile file = files.createTempFile(name, ".zip");
+        files.delete(file);
+        return file;
     }
 
-    private void createZipEntry(ZipOutputStream zip, File root, File file) throws IOException {
-        try (FileInputStream resource = new FileInputStream(file)) {
-            zip.putNextEntry(new ZipEntry(getRelativePath(root, file)));
-            copy(resource, zip);
-        } finally {
-            zip.closeEntry();
-        }
-    }
-
-    private String getRelativePath(File root, File file) {
-        return root.toPath().relativize(file.toPath()).toString();
+    private ZipParameters getZipParameters() {
+        final ZipParameters params = new ZipParameters();
+        params.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
+        params.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
+        return params;
     }
 }
