592990a2b7e3150aab1c4ba115e1620970cf6d5a
Aliaksei Zhytnik <azhyt@sam-solutions.net>
2016-12-05 13:09:44 +0300

    add line by line DirectoryComparator strategy

--- project/cloud-commons/src/main/java/com/sam/jcc/cloud/tool/DirectoryComparator.java
+++ project/cloud-commons/src/main/java/com/sam/jcc/cloud/tool/DirectoryComparator.java
@@ -1,15 +1,19 @@
 package com.sam.jcc.cloud.tool;
 
 import com.google.common.io.Files;
+import com.sam.jcc.cloud.i.Experimental;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.Optional;
 
 import static com.google.common.hash.Hashing.md5;
+import static java.util.Objects.nonNull;
 import static java.util.stream.Collectors.toList;
+import static one.util.streamex.EntryStream.zip;
 
 /**
  * @author Alexey Zhytnik
@@ -20,6 +24,46 @@ public final class DirectoryComparator {
     private FileManager files = new FileManager();
 
     public boolean areEquals(File a, File b) {
+        final Optional<Entry<File, File>> diff =
+                zip(
+                        allFiles(a),
+                        allFiles(b)
+                )
+                        .filter(this::notEquals)
+                        .findAny();
+        return !diff.isPresent();
+    }
+
+    private boolean notEquals(Entry<File, File> pair) {
+        try (BufferedReader first = getReader(pair.getKey());
+             BufferedReader second = getReader(pair.getValue())) {
+
+            String a = "", b = "";
+            boolean equals = true;
+
+            while (equals && ((a = first.readLine()) != null) && ((b = second.readLine()) != null)) {
+                equals = a.equals(b);
+            }
+            if (equals && a == null) b = second.readLine();
+
+            return !equals || nonNull(a) || nonNull(b);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private BufferedReader getReader(File file) throws FileNotFoundException {
+        final FileReader reader = new FileReader(file);
+        return new BufferedReader(reader);
+    }
+
+    @SuppressWarnings("unused")
+    @Experimental(
+                    "Fast hash equality checking by md5 " +
+                    "with the possibility of parallelism, " +
+                    "depends on file format"
+    )
+    private boolean areHashEquals(File a, File b) {
         return hash(allFiles(a)) == hash(allFiles((b)));
     }
 
