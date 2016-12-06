ddbce7c9a78e176dd46252cc68e042097e93e9df
Aliaksei Zhytnik <azhyt@sam-solutions.net>
2016-12-06 12:17:17 +0300

    add Process killer

---
+++ project/cloud-vcs/src/test/java/com/sam/jcc/cloud/vcs/git/ProcessKiller.java
@@ -0,0 +1,90 @@
+package com.sam.jcc.cloud.vcs.git;
+
+import com.jezhumble.javasysmon.JavaSysMon;
+import com.jezhumble.javasysmon.ProcessInfo;
+import com.sun.jna.Pointer;
+import com.sun.jna.platform.win32.Kernel32;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static com.sun.jna.platform.win32.WinNT.HANDLE;
+import static java.util.stream.Collectors.toList;
+import static org.apache.commons.lang3.SystemUtils.IS_OS_UNIX;
+import static org.apache.commons.lang3.SystemUtils.IS_OS_WINDOWS;
+import static org.springframework.util.ReflectionUtils.*;
+
+/**
+ * @author Alexey Zhytnik
+ * @since 05-Dec-16
+ */
+class ProcessKiller {
+
+    private JavaSysMon system;
+
+    public ProcessKiller() {
+        system = new JavaSysMon();
+
+        if (!system.supportedPlatform()) {
+            throw new RuntimeException("Unsupported platform");
+        }
+    }
+
+    /**
+     * Kills the Process and it's child-processes.
+     *
+     * @see <a href="https://github.com/jezhumble/javasysmon">JavaSysMon</a>
+     */
+    public void kill(Process process) {
+        final int pid = getPid(process);
+
+        final List<ProcessInfo> processes = Arrays
+                .stream(system.processTable())
+                .collect(toList());
+
+        process.destroyForcibly();
+        killChildes(processes, pid);
+    }
+
+    private void killChildes(List<ProcessInfo> processes, int parentPid) {
+        processes.stream()
+                .filter(byParentPidFilter(parentPid))
+                .mapToInt(ProcessInfo::getPid)
+                .forEach(pid -> {
+                    system.killProcess(pid);
+                    killChildes(processes, pid);
+                });
+    }
+
+    private Predicate<ProcessInfo> byParentPidFilter(int pid) {
+        return process -> process.getParentPid() == pid;
+    }
+
+    private int getPid(Process process) {
+        if (IS_OS_UNIX) return getPidInUnix(process);
+        if (IS_OS_WINDOWS) return getPidInWindows(process);
+
+        throw new RuntimeException("Unknown OS: " + system.osName());
+    }
+
+    private int getPidInUnix(Process process) {
+        return getFieldValue(process, "pid");
+    }
+
+    private int getPidInWindows(Process process) {
+        final HANDLE handle = new HANDLE();
+
+        long peer = getFieldValue(process, "handle");
+        handle.setPointer(Pointer.createConstant(peer));
+        return Kernel32.INSTANCE.GetProcessId(handle);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> T getFieldValue(Process process, String name) {
+        final Field field = findField(process.getClass(), name);
+        makeAccessible(field);
+        return (T) getField(field, process);
+    }
+}
