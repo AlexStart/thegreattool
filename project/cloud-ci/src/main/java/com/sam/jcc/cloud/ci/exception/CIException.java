8a49142ae6b59691af14d87f2a577ec6a9d46b78
Alexey Zhytnik <alexeyzhytnik@gmail.com>
2016-12-20 11:38:46 +0300

    add Jenkins plugin installer

---
+++ project/cloud-ci/src/main/java/com/sam/jcc/cloud/ci/exception/CIException.java
@@ -0,0 +1,26 @@
+package com.sam.jcc.cloud.ci.exception;
+
+import com.sam.jcc.cloud.exception.CloudException;
+
+/**
+ * @author Alexey Zhytnik
+ * @since 16-Dec-16
+ */
+public class CIException extends CloudException {
+
+    public CIException() {
+        super("ci.error.default");
+    }
+
+    public CIException(Throwable cause) {
+        super("ci.error.default", cause);
+    }
+
+    protected CIException(String key, Object... args) {
+        super(key, args);
+    }
+
+    protected CIException(Throwable cause, String key, Object... args) {
+        super(cause, key, args);
+    }
+}
---
+++ project/cloud-ci/src/main/java/com/sam/jcc/cloud/ci/impl/JenkinsPluginInstaller.java
@@ -0,0 +1,120 @@
+package com.sam.jcc.cloud.ci.impl;
+
+import com.offbytwo.jenkins.JenkinsServer;
+import com.offbytwo.jenkins.client.JenkinsHttpClient;
+import com.offbytwo.jenkins.model.Plugin;
+import com.sam.jcc.cloud.ci.exception.CIException;
+import com.sam.jcc.cloud.exception.NotImplementedCloudException;
+import com.sam.jcc.cloud.i.Experimental;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.google.common.util.concurrent.Uninterruptibles.sleepUninterruptibly;
+import static com.sam.jcc.cloud.PropertyResolver.getProperty;
+import static java.text.MessageFormat.format;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.springframework.util.ReflectionUtils.findField;
+import static org.springframework.util.ReflectionUtils.getField;
+import static org.springframework.util.ReflectionUtils.makeAccessible;
+
+/**
+ * @author Alexey Zhytnik
+ * @since 18-Dec-16
+ */
+@Slf4j
+@Experimental()
+class JenkinsPluginInstaller {
+
+    private static final String INSTALLATION_PATH = "/pluginManager/installNecessaryPlugins";
+
+    private long maxInstallTimeOut;
+
+    private JenkinsServer server;
+    private JenkinsHttpClient client;
+
+    public JenkinsPluginInstaller(JenkinsServer server) {
+        this.server = server;
+        this.client = extractHttpClient(server);
+        maxInstallTimeOut = Long.valueOf(getProperty("jenkins.install.timeout"));
+    }
+
+    public void install(Set<Entry<String, String>> plugins) {
+
+        final Set<Entry<String, String>> pluginsForInstall = plugins.stream()
+                .filter(p -> !isInstalled(p.getKey()))
+                .collect(Collectors.toSet());
+
+        pluginsForInstall.forEach(this::installAndWait);
+
+        if(!pluginsForInstall.isEmpty()) refreshPlugins();
+    }
+
+    private boolean isInstalled(String name) {
+        final List<Plugin> plugins = getPluginManager().getPlugins();
+
+        final Optional<Plugin> plugin = plugins.stream()
+                .filter(p -> name.equals(p.getShortName()))
+                .findAny();
+
+        return plugin.isPresent();
+    }
+
+    public void installAndWait(Entry<String, String> plugin) {
+        install(plugin);
+        waitInstallation(plugin);
+    }
+
+    public void install(Entry<String, String> plugin) {
+        log.info("{0}-{1} will be installed", plugin.getKey(), plugin.getValue());
+
+        try {
+            client.post_xml(
+                    INSTALLATION_PATH,
+                    installPluginQuery(plugin.getKey(), plugin.getValue())
+            );
+        } catch (IOException e) {
+            throw new CIException(e);
+        }
+    }
+
+    private String installPluginQuery(String name, String version) {
+        return format("<jenkins><install plugin=\"{0}@{1}\"/></jenkins>", name, version);
+    }
+
+    private void waitInstallation(Entry<String, String> plugin) {
+        long remaining = maxInstallTimeOut;
+
+        while (!isInstalled(plugin.getKey()) && remaining > 0L) {
+            log.info("Installation waiting of {0}-{1}", plugin.getKey(), plugin.getValue());
+
+            sleepUninterruptibly(1_000L, MILLISECONDS);
+            remaining -= 1_000L;
+        }
+    }
+
+    //TODO: missed something unknown important installation step, need Jenkins restart
+    private void refreshPlugins() {
+        throw new NotImplementedCloudException();
+    }
+
+    private JenkinsHttpClient extractHttpClient(JenkinsServer server) {
+        final Field client = findField(JenkinsServer.class, "client");
+        makeAccessible(client);
+        return (JenkinsHttpClient) getField(client, server);
+    }
+
+    private com.offbytwo.jenkins.model.PluginManager getPluginManager() {
+        try {
+            return server.getPluginManager();
+        } catch (IOException e) {
+            throw new CIException(e);
+        }
+    }
+}
