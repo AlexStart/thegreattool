51ea5a78b2ff986583b7ca9e3cf8878f09e252af
Alexey Zhytnik <alexeyzhytnik@gmail.com>
2016-12-22 23:32:58 +0300

    support CIProvider

---
+++ project/cloud-commons/src/main/java/com/sam/jcc/cloud/i/ci/ICIProvider.java
@@ -0,0 +1,10 @@
+package com.sam.jcc.cloud.i.ci;
+
+import com.sam.jcc.cloud.i.IProvider;
+
+/**
+ * @author Alexey Zhytnik
+ * @since 22-Dec-16
+ */
+public interface ICIProvider extends IProvider<ICIMetadata> {
+}
