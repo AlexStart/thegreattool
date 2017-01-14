bb735b2dc90e50f14be06a483c8032466efe7c04
Alexey Zhytnik <alexeyzhytnik@gmail.com>
2017-01-15 01:19:10 +0300

    support all dependency def

--- project/cloud-utils/src/main/java/com/sam/jcc/cloud/utils/project/GradleDependencyManager.java
+++ project/cloud-utils/src/main/java/com/sam/jcc/cloud/utils/project/GradleDependencyManager.java
@@ -1,5 +1,6 @@
 package com.sam.jcc.cloud.utils.project;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.io.Resources;
 import com.sam.jcc.cloud.exception.InternalCloudException;
 import com.sam.jcc.cloud.utils.project.DependencyManager.Dependency;
@@ -16,8 +17,9 @@ import java.util.regex.Pattern;
 import static com.google.common.base.Charsets.UTF_8;
 import static java.lang.String.format;
 import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 import static java.util.Optional.empty;
-import static java.util.Optional.of;
+import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
 
 /**
@@ -26,14 +28,16 @@ import static java.util.stream.Collectors.toList;
  */
 class GradleDependencyManager implements IDependencyManager<Dependency> {
 
-    private static final Pattern NAME_PATTERN, SCOPE_PATTER, GROUP_PATTERN, VERSION_PATTERN, DEPENDENCIES_PATTERN;
+    private static final Pattern NAME_PATTERN, SCOPE_PATTER, GROUP_PATTERN,
+            VERSION_PATTERN, DEPENDENCIES_PATTERN, SHORT_DEFINITION;
 
     static {
-        SCOPE_PATTER = Pattern.compile("\\b(.+)\\(|\\b(\\S+)");
+        SCOPE_PATTER = Pattern.compile("(.+)\\(|(\\S+)");
         NAME_PATTERN = Pattern.compile("name:\\s*'(.*?)'|name:\\s*\"(.*?)\"");
         GROUP_PATTERN = Pattern.compile("group:\\s*'(.*?)'|group:\\s*\"(.*?)\"");
         VERSION_PATTERN = Pattern.compile("version:\\s*'(.*?)'|version:\\s*\"(.*?)\"");
         DEPENDENCIES_PATTERN = Pattern.compile("dependencies\\s*\\{(((?!dependencies).|\\s)*?)}\\s*");
+        SHORT_DEFINITION = Pattern.compile("((.*)\\([',\"](\\S+):(\\S+):(\\S+)[',\"]\\))|((.*)\\([',\"](\\S+):(\\S+)[',\"]\\))");
     }
 
     @Override
@@ -48,15 +52,6 @@ class GradleDependencyManager implements IDependencyManager<Dependency> {
         return matcher.replaceFirst(format("dependencies { $1 %s}", asString(dependency)));
     }
 
-    private List<Dependency> extractDependencies(String dependencies) {
-        return Arrays
-                .stream(dependencies.split("\t\n|\n"))
-                .map(String::trim)
-                .filter(s -> !s.isEmpty())
-                .map(this::parse)
-                .collect(toList());
-    }
-
     private Matcher getDependenciesMatcher(File file) {
         final String build = asString(file);
         final Matcher matcher = DEPENDENCIES_PATTERN.matcher(build);
@@ -67,14 +62,47 @@ class GradleDependencyManager implements IDependencyManager<Dependency> {
         return matcher;
     }
 
+    @VisibleForTesting List<Dependency> extractDependencies(String dependencies) {
+        return Arrays
+                .stream(dependencies.split("\t\n|\n"))
+                .map(String::trim)
+                .filter(s -> !s.isEmpty())
+                .map(this::parse)
+                .collect(toList());
+    }
+
     private Dependency parse(String s) {
         final Dependency d = new Dependency();
 
         getNonNullGroup(SCOPE_PATTER, s).ifPresent(d::setScope);
-        getNonNullGroup(GROUP_PATTERN, s).ifPresent(d::setGroupId);
-        getNonNullGroup(NAME_PATTERN, s).ifPresent(d::setArtifactId);
-        getNonNullGroup(VERSION_PATTERN, s).ifPresent(d::setVersion);
+
+        if (isShotDefinition(s)) {
+            fillShort(d, s);
+        } else {
+            getNonNullGroup(GROUP_PATTERN, s).ifPresent(d::setGroupId);
+            getNonNullGroup(NAME_PATTERN, s).ifPresent(d::setArtifactId);
+            getNonNullGroup(VERSION_PATTERN, s).ifPresent(d::setVersion);
+        }
         return d;
+    }
+
+    private void fillShort(Dependency d, String s) {
+        final Matcher m = SHORT_DEFINITION.matcher(s);
+
+        if (!m.find()) throw new InternalCloudException();
+
+        if (nonNull(m.group(3))) {
+            d.setGroupId(m.group(3));
+            d.setArtifactId(m.group(4));
+            d.setVersion(m.group(5));
+        } else {
+            d.setGroupId(m.group(8));
+            d.setArtifactId(m.group(9));
+        }
+    }
+
+    private boolean isShotDefinition(String def) {
+        return def.endsWith(")");
     }
 
     private Optional<String> getNonNullGroup(Pattern pattern, String s) {
@@ -86,7 +114,7 @@ class GradleDependencyManager implements IDependencyManager<Dependency> {
         if (isNull(group)) {
             group = matcher.group(2);
         }
-        return of(group);
+        return ofNullable(group);
     }
 
     private String asString(Dependency dependency) {
---
+++ project/cloud-utils/src/test/java/com/sam/jcc/cloud/utils/project/GradleDependencyManagerTest.java
@@ -0,0 +1,46 @@
+package com.sam.jcc.cloud.utils.project;
+
+import com.sam.jcc.cloud.utils.project.DependencyManager.Dependency;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+/**
+ * @author Alexey Zhytnik
+ * @since 14-Jan-17
+ */
+public class GradleDependencyManagerTest {
+
+    GradleDependencyManager manager = new GradleDependencyManager();
+
+    @Test
+    public void getsDependencies() {
+        final List<Dependency> dependencies = manager.extractDependencies(
+                "testCompile group: 'com.junit', name: 'junit', version: \"4.11\""
+        );
+
+        assertThat(dependencies).hasSize(1);
+        final Dependency d = dependencies.get(0);
+
+        assertThat(d.getVersion()).isEqualTo("4.11");
+        assertThat(d.getArtifactId()).isEqualTo("junit");
+        assertThat(d.getGroupId()).isEqualTo("com.junit");
+        assertThat(d.getScope()).isEqualTo("testCompile");
+    }
+
+    @Test
+    public void getsDependenciesInShortStyle() {
+        final List<Dependency> dependencies = manager.extractDependencies(
+                "compile('org.springframework.boot:spring-boot-starter-web\")"
+        );
+
+        assertThat(dependencies).hasSize(1);
+        final Dependency d = dependencies.get(0);
+
+        assertThat(d.getScope()).isEqualTo("compile");
+        assertThat(d.getGroupId()).isEqualTo("org.springframework.boot");
+        assertThat(d.getArtifactId()).isEqualTo("spring-boot-starter-web");
+    }
+}
