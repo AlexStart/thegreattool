12af9c5937c71c6e5b9cb76cd4c7d9075f3c05d5
Alexey Zhytnik <alexeyzhytnik@gmail.com>
2017-01-16 23:59:42 +0300

    table name validator tests

--- project/cloud-persistence/src/main/java/com/sam/jcc/cloud/dataprovider/impl/TableNameValidator.java
+++ project/cloud-persistence/src/main/java/com/sam/jcc/cloud/dataprovider/impl/TableNameValidator.java
@@ -3,6 +3,7 @@ package com.sam.jcc.cloud.dataprovider.impl;
 import com.sam.jcc.cloud.exception.BusinessCloudException;
 import org.springframework.stereotype.Component;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -12,10 +13,12 @@ import java.util.regex.Pattern;
 @Component
 class TableNameValidator {
 
-    private static final Pattern TABLE_NAME_PATTERN = Pattern.compile("[0-9a-zA-Z$_]+");
+    private static final Pattern TABLE_NAME_PATTERN = Pattern.compile("[0-9a-zA-Z$_]+$");
 
     public void validate(String name) {
-        if (!TABLE_NAME_PATTERN.matcher(name).find()) {
+        final Matcher matcher = TABLE_NAME_PATTERN.matcher(name);
+
+        if (!matcher.find() || !matcher.group(0).equals(name)) {
             throw new TableNameValidationException(name);
         }
     }
