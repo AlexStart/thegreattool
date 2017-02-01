ea4cc7f934e301f9b16f1657ff5f601f42bf0fd6
Aleg Katovich <a.katovich@sam-solutions.com>
2017-02-01 14:32:08 +0300

    Testing...

--- project/cloud-initializr-adapter/src/main/java/com/sam/jcc/cloud/project/SourceProcessor.java
+++ project/cloud-initializr-adapter/src/main/java/com/sam/jcc/cloud/project/SourceProcessor.java
@@ -7,12 +7,14 @@ import org.springframework.stereotype.Component;
 
 import io.spring.initializr.generator.ProjectGenerator;
 import io.spring.initializr.generator.ProjectRequest;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author Alexey Zhytnik
  * @since 15.11.2016
  */
 @Component
+@Slf4j
 class SourceProcessor {
 
 	@Autowired
@@ -24,6 +26,7 @@ class SourceProcessor {
 	public void process(ProjectMetadata project) {
 		final ProjectRequest request = converter.convert(project);
 		File srcDir = generator.generateProjectStructure(request);
+		log.info("Generated project structure in " + srcDir.getAbsolutePath());
 		project.setDirectory(srcDir);
 	}
 }
