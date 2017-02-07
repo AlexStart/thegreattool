b1297841de63f018e2ae94e034f71166d916c6de
Aleg Katovich <a.katovich@sam-solutions.com>
2017-02-07 16:49:48 +0300

    Minor fixes

--- project/cloud-utils/src/main/java/com/sam/jcc/cloud/utils/project/ArtifactIdValidator.java
+++ project/cloud-utils/src/main/java/com/sam/jcc/cloud/utils/project/ArtifactIdValidator.java
@@ -1,9 +1,10 @@
 package com.sam.jcc.cloud.utils.project;
 
-import com.sam.jcc.cloud.exception.BusinessCloudException;
+import static java.util.Objects.isNull;
+
 import org.springframework.stereotype.Component;
 
-import static java.util.Objects.isNull;
+import com.sam.jcc.cloud.exception.BusinessCloudException;
 
 /**
  * @author Alexey Zhytnik
@@ -12,17 +13,22 @@ import static java.util.Objects.isNull;
 @Component
 public class ArtifactIdValidator {
 
-    private static final String ARTIFACT_REGEX = "[A-Za-z0-9_\\-.]+";
+	private static final String ARTIFACT_REGEX = "[A-Za-z0-9_\\-.]+";
 
-    public void validate(String s) {
-        if (isNull(s)) throw new AppMetadataValidationException();
-        if (!s.trim().toLowerCase().equals(s)) throw new AppMetadataValidationException();
-        if (!s.matches(ARTIFACT_REGEX)) throw new AppMetadataValidationException();
-    }
+	public void validate(String s) {
+		if (isNull(s))
+			throw new AppMetadataValidationException();
+		if (!s.trim().toLowerCase().equals(s))
+			throw new AppMetadataValidationException();
+		if (!s.matches(ARTIFACT_REGEX))
+			throw new AppMetadataValidationException();
+		if (s.startsWith("cloud") || s.startsWith("sam-checkstyle"))
+			throw new AppMetadataValidationException();
+	}
 
-    public static class AppMetadataValidationException extends BusinessCloudException {
-        public AppMetadataValidationException() {
-            super("project.artifactId.notValid");
-        }
-    }
+	public static class AppMetadataValidationException extends BusinessCloudException {
+		public AppMetadataValidationException() {
+			super("project.artifactId.notValid");
+		}
+	}
 }
